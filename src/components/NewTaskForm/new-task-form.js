345441ecbe9258304ffc6783b213225b8a17209d
ShevPa <shevsdenk@mail.ru>
2023-09-21 21:08:33 +0400

    fix

--- src/components/NewTaskForm/new-task-form.js
+++ src/components/NewTaskForm/new-task-form.js
@@ -33,6 +33,7 @@ export default class NewTaskForm extends React.Component {
             type="text"
             className="new-todo"
             placeholder="What needs to be done?"
+            required
             autoFocus
             onChange={this.onLabelChange}
             value={this.state.label}
